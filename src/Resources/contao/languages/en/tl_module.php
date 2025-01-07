d92bd1e3c619873d3650b074e3759f5e437b9bde
Philipp Lorenz <philipp.lorenz@etes.de>
2025-01-07 17:15:35 +0100

    Bump to version 1.1 (Contao 5.3+ compatibility)

--- src/Resources/contao/languages/en/tl_module.php
+++ src/Resources/contao/languages/en/tl_module.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 /**
  * Language strings for the tl_module DCA fields
  */
-$GLOBALS['TL_LANG']['tl_module']['cookiebot_declaration_template'] = array (
+$GLOBALS['TL_LANG']['tl_module']['ccm19_declaration_template'] = array (
     'Custom module template',
     'Here you can overwrite the default module template.'
     );
